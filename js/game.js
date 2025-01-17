9be687abfacc18f0c16150e41f8efc3d1245867c
Caryen Song <caryensong@googlemail.com>
2025-01-17 21:17:41 +0100

    add class file -> movable.object.class.js

--- js/game.js
+++ js/game.js
@@ -1,12 +1,12 @@
 let canvas;
 let ctx; 
-let character = new Image();
+let character = new MovableObject();
 
 function init(){
     canvas =document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    character.src = './img7human/Walking70_Fallen_Angels_Walking_000.png';
-    ctx.drawImage(character, 30, 30, 50, 50);
+console.log('My Character', character);
+
 
 }
