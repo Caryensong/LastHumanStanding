b08a15ec839dbb1d97c56c8fda977a20af65cca7
Caryen Song <caryensong@googlemail.com>
2025-01-18 21:50:06 +0100

    add draw() for the canva and requestAnimationFrame()

--- models/world.class.js
+++ models/world.class.js
@@ -6,14 +6,29 @@ class World {
     new Zombies()
 ];
 
+  canvas;
   ctx;
+
   constructor(canvas){
     this.ctx = canvas.getContext('2d');
+    this.canvas = canvas;
     this.draw();
 }
 
 draw(){
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        
+    this.enemies.forEach(enemy => {
+      this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+    });
+
+
+
+    let self = this;
+    requestAnimationFrame(function(){
+      self.draw();
+    })
     }
 }
