dd6f5015b687197d1badc3dd18d268305e5d9a63
Caryen Song <caryensong@googlemail.com>
2025-01-18 22:57:23 +0100

    update zombie img

--- models/zombie.class.js
+++ models/zombie.class.js
@@ -2,6 +2,8 @@ class Zombies extends MovableObject{
 
     constructor(){
         super().loadImage('./img/zombie1/Walking/0_Zombie_Villager_Walking_000.png');
+
+        this.x = 200 + Math.random()* 500;
     }
 
 }
