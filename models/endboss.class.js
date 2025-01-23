8acd12d4c4b4a8394b42894bdef60b339106f863
Caryensong <caryensong@googlemail.com>
2025-01-23 21:54:32 +0100

    Enable Endboss to move and play slashing animation when player enters a 400px range

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,40 +1,34 @@
 class Endboss extends MovableObject {
   walking_sound = new Audio("./audio/monster_step.mp3");
 
-  offset={
+  offset = {
     top: 65,
-    left:90,
-    right:90, 
-    bottom:40
-}
+    left: 90,
+    right: 60,
+    bottom: 40
+  }
 
-  Images_Walking = [
-    "./img/endboss/Walking/0_Golem_Walking_000.png",
-    "./img/endboss/Walking/0_Golem_Walking_001.png",
-    "./img/endboss/Walking/0_Golem_Walking_002.png",
-    "./img/endboss/Walking/0_Golem_Walking_003.png",
-    "./img/endboss/Walking/0_Golem_Walking_004.png",
-    "./img/endboss/Walking/0_Golem_Walking_005.png",
-    "./img/endboss/Walking/0_Golem_Walking_006.png",
-    "./img/endboss/Walking/0_Golem_Walking_007.png",
-    "./img/endboss/Walking/0_Golem_Walking_008.png",
-    "./img/endboss/Walking/0_Golem_Walking_009.png",
-    "./img/endboss/Walking/0_Golem_Walking_010.png",
-    "./img/endboss/Walking/0_Golem_Walking_011.png",
-    "./img/endboss/Walking/0_Golem_Walking_012.png",
-    "./img/endboss/Walking/0_Golem_Walking_013.png",
-    "./img/endboss/Walking/0_Golem_Walking_014.png",
-    "./img/endboss/Walking/0_Golem_Walking_015.png",
-    "./img/endboss/Walking/0_Golem_Walking_016.png",
-    "./img/endboss/Walking/0_Golem_Walking_017.png",
-    "./img/endboss/Walking/0_Golem_Walking_018.png",
-    "./img/endboss/Walking/0_Golem_Walking_019.png",
+  Images_Slashing = [
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_000.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_001.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_002.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_003.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_004.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_005.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_006.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_007.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_008.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_009.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_010.png',
+    'img/endboss/Run Slashing/0_Golem_Run Slashing_011.png',
   ];
 
+  hadFirstContact = false;
+
   constructor() {
-    super().loadImage(this.Images_Walking[0]);
-    this.loadImages(this.Images_Walking);
-    this.x = 700;
+    super().loadImage(this.Images_Slashing[0]);
+    this.loadImages(this.Images_Slashing);
+    this.x = 1800;
     this.y = 175;
     this.height = 300;
     this.width = 305;
@@ -42,16 +36,27 @@ class Endboss extends MovableObject {
     this.otherDirection = true;
     this.animate();
   }
-  
+
   animate() {
-    setInterval(()=>{     
-      this.moveLeft();
-  }, 1000/ 60);
+
+    //   setInterval(()=>{     
+    //     this.moveLeft();
+    // }, 1000/ 60);
+
 
 
     setInterval(() => {
-      this.playAnimation(this.Images_Walking);
-      this.walking_sound.play();
+      const distance = Math.abs(world.character.x - this.x);
+      if (distance < 400) {
+        setInterval(() => {
+          this.moveLeft();
+        }, 1000 / 25);
+
+        this.playAnimation(this.Images_Slashing);
+        this.walking_sound.play();
+        this.hadFirstContact = true;
+      }
+
     }, 100);
   }
 }
