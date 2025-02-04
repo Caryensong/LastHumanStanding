10591b027d1b131442351c89edcb130cd9b5a634
Caryensong <caryensong@googlemail.com>
2025-02-04 16:02:42 +0100

    update gamoeover()

--- js/game.js
+++ js/game.js
@@ -2,108 +2,105 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-function stopGame(world) {
-    clearInterval(world.intervalID);
-    world.throwableObjects = []; // Alle geworfenen Objekte entfernen
-}
-
-function startGame(){
-   startScreen = document.getElementById("howToPlayBox");
+function startGame() {
+    startScreen = document.getElementById("howToPlayBox");
     startScreen.classList.add("d-none");
-     startScreen = "";
-     canvas =document.getElementById("canvas");
-     canvas.classList.remove("d-none");
-     initLevel();
-     world = new World(canvas, keyboard);
+    startScreen = "";
+
+    canvas = document.getElementById("canvas");
+    canvas.classList.remove("d-none");
+    initLevel();
+    world = new World(canvas, keyboard);
+    world.gameOver = false;
 }
 
-function howToPlayScreen(){
+function howToPlayScreen() {
     descriptionScreen = document.getElementById("descriptionBox");
     descriptionScreen.classList.add("d-none");
     startScreen = document.getElementById("startScreen");
     startScreen.innerHTML = howToPlayTemplate();
 }
 
-function startDescription(){
+function startDescription() {
     firstScreen = document.getElementById("startContent");
     firstScreen.classList.add("d-none");
     startScreen = document.getElementById("startScreen");
     startScreen.innerHTML = descriptionTemplate();
 }
 
-function init(){
+function init() {
     startScreen = document.getElementById("startScreen");
-    startScreen.innerHTML= "";
-    endScreen = document.getElementById("endScreenBox"); 
+    startScreen.innerHTML = "";
+    endScreen = document.getElementById("endScreenBox");
     canvas = document.getElementById('canvas');
-   
-    if( endScreen){
-         endScreen.classList.add("d-none");  
-         canvas.classList.remove("d-none");
+
+    if (endScreen) {
+        endScreen.classList.add("d-none");
+        canvas.classList.remove("d-none");
     }
     startScreen.innerHTML = startTemplate();
 }
 
 
 window.addEventListener("keydown", (e) => {
-     if(e.keyCode == 39){
+    if (e.keyCode == 39) {
         keyboard.RIGHT = true;
     }
 
-    if(e.keyCode == 37){
+    if (e.keyCode == 37) {
         keyboard.LEFT = true;
     }
 
-    if(e.keyCode == 38){
+    if (e.keyCode == 38) {
         keyboard.UP = true;
     }
 
-    if(e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = true;
     }
-    if(e.keyCode == 32){
+    if (e.keyCode == 32) {
         keyboard.SPACE = true;
     }
-    if(e.keyCode == 68){
+    if (e.keyCode == 68) {
         keyboard.D = true;
     }
-    if(e.keyCode == 83){
+    if (e.keyCode == 83) {
         keyboard.S = true;
     }
-    if(e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = true;
     }
 
 });
 
 window.addEventListener("keyup", (e) => {
-    if(e.keyCode == 39){
+    if (e.keyCode == 39) {
         keyboard.RIGHT = false;
     }
 
-    if(e.keyCode == 37){
+    if (e.keyCode == 37) {
         keyboard.LEFT = false;
     }
 
-    if(e.keyCode == 38){
+    if (e.keyCode == 38) {
         keyboard.UP = false;
     }
-    if(e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = false;
     }
-    if(e.keyCode == 32){
-       keyboard.SPACE_SOLVED = false;
-       keyboard.SPACE = false;
-    }   
-    if(e.keyCode == 68){
+    if (e.keyCode == 32) {
+        keyboard.SPACE_SOLVED = false;
+        keyboard.SPACE = false;
+    }
+    if (e.keyCode == 68) {
         keyboard.D_SOLVED = false;
         keyboard.D = false;
     }
-    if(e.keyCode == 83){
+    if (e.keyCode == 83) {
         keyboard.S_SOLVED = false;
         keyboard.S = false;
     }
-    if(e.keyCode == 40){
+    if (e.keyCode == 40) {
         keyboard.DOWN = false;
     }
 });
--- models/world.class.js
+++ models/world.class.js
@@ -1,6 +1,6 @@
-class World { 
-  character = new Character(); 
-  level= level1;
+class World {
+  character = new Character();
+  level = level1;
   endboss = new Endboss();
   canvas;
   ctx;
@@ -12,230 +12,224 @@ class World {
   poisonCount = 5; // Maximale Anzahl an Flaschen
   gameOver = false;
 
-  constructor(canvas, keyboard){
+  constructor(canvas, keyboard) {
     this.ctx = canvas.getContext('2d');
     this.canvas = canvas;
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
-    this.checkCollisions(); 
+    this.checkCollisions();
     this.check();
-   
-}
 
-setWorld(){
-  this.character.world = this;
-}
+  }
 
-check(){
-  this.intervalID = setInterval(() => { 
+  setWorld() {
+    this.character.world = this;
+  }
+
+  check() {
+    if (this.intervalID) {
+      clearInterval(this.intervalID);  // Vorheriges Intervall beenden
+    }
+    this.intervalID = setInterval(() => {
+      this.checkCollisions();
+      this.checkThrowObjects();
+      this.checkObjectsColliding();
+      this.checkSlashingCollisions();
+    }, 200);
+  }
+
+  checkThrowObjects() {
+    if (this.keyboard.D && this.poisonCount > 0) {
+      let bottle = new ThrowableObject(this.character.x + 60, this.character.y + 50, this.character);
+      this.throwableObjects.push(bottle);
+      this.poisonCount--;
+      this.updatePoisonBar();
+    }
+  }
+
+  checkObjectsColliding() {
+    this.level.objects.forEach((object, index) => {
+      if (object && this.character.isColliding(object)) {
+        this.handleObjectCollision(object, index);
+      }
+    });
+  }
+
+  handleObjectCollision(object, index) {
+    if (object instanceof PoisonObjects) {
+      console.log('Poison Flasche eingesammelt');
+      this.level.objects.splice(index, 1);
+      this.poisonCount = Math.min(this.poisonCount + 1, 5);
+      this.updatePoisonBar();
+    }      // Life-Objekt einsammeln
+    if (object instanceof LifeObjects) {
+      console.log('Leben eingesammelt');
+      this.level.objects.splice(index, 1);
+      this.character.energy = Math.min(this.character.energy + 20, 100);
+      this.updateLifeBar();
+    }
+  }
+
+  updatePoisonBar() {
+    const poisonBar = this.level.statusBar.find((bar) => bar.type === 'poison');
+    if (poisonBar) {
+      let poisonPercentage = (this.poisonCount / 5) * 100;
+      poisonBar.setPoisonPercentage(poisonPercentage);
+      console.log('Poison-Bar aktualisiert:', poisonPercentage);
+    }
+  }
+
+  updateLifeBar() {
+    const lifeBar = this.level.statusBar.find((bar) => bar.type === 'life');
+    if (lifeBar) {
+      lifeBar.setPercentage(this.character.energy);
+      console.log('Life-Bar aktualisiert:', this.character.energy);
+    }
+  }
+
+  updateEndbossLifeBar() {
+    const lifeBar = this.level.statusBar.find((bar) => bar.type === 'endbossLife');
+    if (lifeBar) {
+      lifeBar.setEndbossPercentage(this.endboss.energy);
+    }
+  }
+
+  checkCollisions() {
+    this.level.enemies.forEach((enemy, index) => {
+      if (this.character.isColliding(enemy) || this.character.isColliding(this.endboss)) {
+        console.log(this.character.isColliding(this.endboss));
+        if (this.character.isSlashing) {
+          console.log("Slashing verhindert Verletzung.");
+          this.character.isInvulnerable = true;
+          if (!this.character.slashTimeout) {
+            this.character.slashTimeout = setTimeout(() => {
+              this.character.isInvulnerable = true;
+              this.character.isSlashing = false;
+              this.character.isInvulnerable = false;
+              this.character.slashTimeout = null;
+            }, 300);
+          }
+          return; // Kein Schaden, wenn Slashing aktiv ist
+        }
+        if (this.character.isTopZombieColliding(enemy) && this.character.isAboveGround()) {
+          this.character.isInvulnerable = true;
+          this.removeEnemy(enemy, index);
+          console.log("Zombie von oben getroffen!");
+
+          setTimeout(() => {
+            this.character.isInvulnerable = false; // Nach kurzer Zeit wieder verwundbar
+          }, 900);
+        }
+
+        if (!this.character.isInvulnerable) {
+          this.character.hit(); // Charakter wird getroffen
+          console.log("Charakter wurde getroffen", this.character.energy);
+          this.updateLifeBar();
+        }
+      }
+    });
+    this.throwableObjects.forEach((bottle, bottleIndex) => {
+      this.handleThrowableObjectCollision(bottle, bottleIndex);
+    });
+
+  }
+
+  handleThrowableObjectCollision(bottle, bottleIndex) {
+    this.level.enemies.forEach((enemy, enemyIndex) => {
+      if (bottle.isColliding(enemy)) {
+        console.log("Poison trifft Zombie!");
+        this.removeThrowableObject(bottleIndex);
+        this.removeEnemy(enemy, enemyIndex);
+      }
+    });
+
+    if (this.endboss && bottle.isColliding(this.endboss)) {
+      console.log("Poison trifft Endboss");
+      this.removeThrowableObject(bottleIndex);
+      this.handleEndbossCollision();
+    }
+  }
+
+  removeThrowableObject(bottleIndex) {
+    this.throwableObjects.splice(bottleIndex, 1);
+  }
+
+  removeEnemy(enemy, index) {
+    enemy.playPoisonDeadAnimation(() => {
+      this.level.enemies.splice(index, 1); // Zombie entfernen
+    });
+  }
+
+  handleEndbossCollision() {
+    this.endboss.playHurtAnimation();
+    this.endboss.hit();
+    console.log("Endboss Life", this.endboss.energy);
+    this.updateEndbossLifeBar();
+  }
+
+  checkSlashingCollisions() {
+    if (this.endboss.isDying) return; // Falls Endboss schon stirbt, keine weitere Aktion
+    if (this.character.isColliding(this.endboss) && this.character.isSlashing) {
+      console.log("Endboss wurde mit Schwert getroffen");
+      this.handleEndbossCollision();
+    } else if (this.character.isColliding(this.endboss)) {
+      this.character.hit(); // Charakter wird getroffen
+      console.log("Charakter wurde getroffen", this.character.energy);
+      this.updateLifeBar();
+    }
+
+    this.level.enemies.forEach((enemy, index) => {
+      if (this.character.isColliding(enemy) && this.character.isSlashingColliding(enemy)) {
+        console.log("Zombie wurde vom Schwert getroffen!");
+        this.removeEnemy(enemy, index);
+      }
+    });
+  }
+
+  checkGameOver() {
+    if (this.character.isDead() && !this.gameOver) {
+      let winner = "endboss";
+      this.gameOver = true;
+      this.renderGameOver(winner);
+
+    } else if (this.endboss.isDead() && !this.gameOver) {
+      let winner = "character";
+      this.gameOver = true;
+
+      this.renderGameOver(winner);
+    }
+  }
+
+  renderGameOver(winner) {
+    console.log("Game Over Screen wird gerendert für:", winner);
+    canvas = document.getElementById("canvas");
+    canvas.classList.add("d-none");
+    let endScreen = document.getElementById("startScreen");
+    if (winner == "character") {
+      endScreen.innerHTML = wonGameTemplate();
+    } else {
+      endScreen.innerHTML = lostGameTemplate();
+    }
+  }
+
+  // checkCharacterMovement() {
+  //   if (this.keyboard.DOWN) {
+  //       this.character.moveDown();
+  //       this.camera_y += this.character.speed; // Kamera nach unten bewegen
+  //   }
+  // }
+
+
+  draw() {
     if (this.gameOver) {
-      clearInterval(this.intervalID);
       return;
     }
-    this.checkCollisions(); 
-    this.checkThrowObjects();
-    this.checkObjectsColliding();
-    this.checkSlashingCollisions(); 
-  }, 200);
-}
-
-checkThrowObjects() {
-  if (this.keyboard.D && this.poisonCount > 0) {
-      let bottle = new ThrowableObject(this.character.x + 60, this.character.y + 50, this.character);
-          this.throwableObjects.push(bottle);
-          this.poisonCount--;
-          this.updatePoisonBar();
-  }
-}
-
-checkObjectsColliding() {
-  this.level.objects.forEach((object, index) => {
-    if (object && this.character.isColliding(object)) {
-      this.handleObjectCollision(object, index);
-    }
-  });
-}
-
-handleObjectCollision(object, index) {
-  if (object instanceof PoisonObjects) {
-    console.log('Poison Flasche eingesammelt');
-    this.level.objects.splice(index, 1);
-    this.poisonCount = Math.min(this.poisonCount +1, 5);
-    this.updatePoisonBar();
- }      // Life-Objekt einsammeln
-  if (object instanceof LifeObjects) {
-    console.log('Leben eingesammelt');
-    this.level.objects.splice(index, 1);
-    this.character.energy = Math.min(this.character.energy + 20, 100);
-    this.updateLifeBar();
-  }
-}
-
-updatePoisonBar() {
-  const poisonBar = this.level.statusBar.find((bar) => bar.type === 'poison');
-  if (poisonBar) {
-    let poisonPercentage = (this.poisonCount / 5) * 100;
-    poisonBar.setPoisonPercentage(poisonPercentage);
-    console.log('Poison-Bar aktualisiert:', poisonPercentage);
-  }
-}
-
-updateLifeBar() {
-  const lifeBar = this.level.statusBar.find((bar) => bar.type === 'life');
-  if (lifeBar) {
-    lifeBar.setPercentage(this.character.energy);
-    console.log('Life-Bar aktualisiert:', this.character.energy);
-  }
-}
-
-updateEndbossLifeBar() {
-  const lifeBar = this.level.statusBar.find((bar) => bar.type === 'endbossLife');
-  if (lifeBar) {
-    lifeBar.setEndbossPercentage(this.endboss.energy);
-  }
-}
-
-checkCollisions() {
-  this.level.enemies.forEach((enemy, index) => {    
-    if (this.character.isColliding(enemy) || this.character.isColliding(this.endboss) ) {
-      console.log(this.character.isColliding(this.endboss));
-      if (this.character.isSlashing) {
-        console.log("Slashing verhindert Verletzung.");
-        this.character.isInvulnerable = true; 
-        if (!this.character.slashTimeout) {
-          this.character.slashTimeout = setTimeout(() => {
-            this.character.isInvulnerable = true; 
-            this.character.isSlashing = false;
-            this.character.isInvulnerable = false;
-            this.character.slashTimeout = null;
-          }, 300);
-        }
-        return; // Kein Schaden, wenn Slashing aktiv ist
-      }
-    if (this.character.isTopZombieColliding(enemy) && this.character.isAboveGround()) {
-        this.character.isInvulnerable = true; 
-        this.removeEnemy(enemy, index);
-        console.log("Zombie von oben getroffen!");
-  
-        setTimeout(() => {
-        this.character.isInvulnerable = false; // Nach kurzer Zeit wieder verwundbar
-        }, 900 );
-      }
-  
-      if (!this.character.isInvulnerable) {
-        this.character.hit(); // Charakter wird getroffen
-        console.log("Charakter wurde getroffen", this.character.energy);
-        this.updateLifeBar();
-      }
-}
-});
-  this.throwableObjects.forEach((bottle, bottleIndex) => {
-    this.handleThrowableObjectCollision(bottle, bottleIndex);
-  });
-
-}
-
-handleThrowableObjectCollision(bottle, bottleIndex) {
-  this.level.enemies.forEach((enemy, enemyIndex) => {
-    if (bottle.isColliding(enemy)) {
-      console.log("Poison trifft Zombie!");
-      this.removeThrowableObject(bottleIndex);
-      this.removeEnemy(enemy, enemyIndex);
-    }
-  });
-
-  if (this.endboss && bottle.isColliding(this.endboss)) {
-    console.log("Poison trifft Endboss");
-    this.removeThrowableObject(bottleIndex);
-    this.handleEndbossCollision();
-  }
-}
-
-removeThrowableObject(bottleIndex) {
-  this.throwableObjects.splice(bottleIndex, 1);
-}
-
-removeEnemy(enemy, index) {
-  enemy.playPoisonDeadAnimation(() => {
-    this.level.enemies.splice(index, 1); // Zombie entfernen
-  });
-}
-
-handleEndbossCollision() {
-  this.endboss.playHurtAnimation();
-  this.endboss.hit();
-  console.log("Endboss Life", this.endboss.energy);
-  this.updateEndbossLifeBar();
-}
- 
-checkSlashingCollisions() {
-  if (this.endboss.isDying) return; // Falls Endboss schon stirbt, keine weitere Aktion
-  if (this.character.isColliding(this.endboss) && this.character.isSlashing) {
-    console.log("Endboss wurde mit Schwert getroffen");
-    this.handleEndbossCollision();
-  } else if(this.character.isColliding(this.endboss)){
-    this.character.hit(); // Charakter wird getroffen
-    console.log("Charakter wurde getroffen", this.character.energy);
-    this.updateLifeBar();
-  }
-
-  this.level.enemies.forEach((enemy, index) => {
-    if (this.character.isColliding(enemy) && this.character.isSlashingColliding(enemy)) {
-      console.log("Zombie wurde vom Schwert getroffen!");
-      this.removeEnemy(enemy, index);
-    }
-  });
-}
-
-checkGameOver(){
-  if(this.character.isDead()){
-    let winner = "endboss";
-    setTimeout(()=> {
-      this.gameOver = true;
-      stopGame(this);
-      this.renderGameOver(winner);
-
-    },2500);
-  } else if(this.endboss.isDead()){
-    let winner = "character";
-    setTimeout(()=> {
-      this.gameOver = true; 
-      stopGame(this);
-      this.renderGameOver(winner);
-    },2500);
-  }
-}
-
-renderGameOver(winner){
-  console.log("Game Over Screen wird gerendert für:", winner);
-  canvas = document.getElementById("canvas");
-  canvas.classList.add("d-none");
-  let endScreen = document.getElementById("startScreen");
-  if(winner == "character"){
-    endScreen.innerHTML = wonGameTemplate();
-  } else {
-    endScreen.innerHTML = lostGameTemplate();
-  }
-}
-
-// checkCharacterMovement() {
-//   if (this.keyboard.DOWN) {
-//       this.character.moveDown();
-//       this.camera_y += this.character.speed; // Kamera nach unten bewegen
-//   }
-// }
-
-
-draw(){
-  if(this.gameOver){
-    return;
-  }
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectToMap(this.level.backgroundObjects);   
-    this.addObjectToMap( this.level.moon);
+    this.addObjectToMap(this.level.backgroundObjects);
+    this.addObjectToMap(this.level.moon);
     this.addObjectToMap(this.level.clouds);
     this.addObjectToMap(this.level.hand);
     this.addObjectToMap(this.level.start);
@@ -246,7 +240,7 @@ draw(){
 
     this.level.statusBar.forEach((statusBar) => {
       this.addToMap(statusBar); // Jede StatusBar einzeln hinzufügen
-  });
+    });
 
     this.ctx.translate(this.camera_x, 0);   //camera forwards
 
@@ -259,47 +253,47 @@ draw(){
     this.checkGameOver();
     if (!this.gameOver) { // Hier prüfen, ob das Spiel weiterläuft
       let self = this;
-      requestAnimationFrame(function(){
+      requestAnimationFrame(function () {
         self.draw();
       });
     }
   }
 
-  addObjectToMap(objects){
+  addObjectToMap(objects) {
     objects.forEach(o => {
       this.addToMap(o);
     });
 
   }
 
-  addToMap(mo){
+  addToMap(mo) {
     if (mo instanceof BackgroundObject || mo instanceof Cloud) {
       this.ctx.globalAlpha = mo.opacity;
     } else {
       this.ctx.globalAlpha = 1; // Full opacity for other objects
     }
 
-    if(mo.otherDirection){
-    this.flipImage(mo);
+    if (mo.otherDirection) {
+      this.flipImage(mo);
     }
-  
+
     mo.draw(this.ctx);
     mo.drawFrame(this.ctx);
 
-    if(mo.otherDirection){
-    this.flipImageBack(mo);
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
     }
   }
 
-  flipImage(mo){
+  flipImage(mo) {
     this.ctx.save();
     this.ctx.translate(mo.width, 0);
     this.ctx.scale(-1, 1);
-    mo.x =mo.x *  -1;
+    mo.x = mo.x * -1;
   }
 
-  flipImageBack(mo){
+  flipImageBack(mo) {
     this.ctx.restore();  //macht die Spiegelung wieder
-    mo.x =mo.x *  -1;
+    mo.x = mo.x * -1;
   }
 }
